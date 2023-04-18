5dad97105c1d292953e2a5748bceaba6c029594f
Luciano Filho <lvgalvaofilho@gmail.com>
2023-04-18 04:35:46 -0300

    ajuste na docstring

--- intro_testes/conversao.py
+++ intro_testes/conversao.py
@@ -1,17 +1,17 @@
 """
 Esse programa faz conversões.
-
->>> conversao(32)
-0.0
-
->>> conversao(-40)
--40.0
 """
 
 def conversao(temperatura_fahrenheit):
 
     """
     Método que converte
-    
+
+    >>> conversao(32)
+    0.0
+
+    >>> conversao(-40)
+    -40.0
     """
+
     return 5 * ((temperatura_fahrenheit - 32) / 9)
