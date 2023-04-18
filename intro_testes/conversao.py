46a9f3727604612392279811ec1b34ed00fce9a3
Luciano Filho <lvgalvaofilho@gmail.com>
2023-04-18 04:33:52 -0300

    first commit

---
+++ intro_testes/conversao.py
@@ -0,0 +1,17 @@
+"""
+Esse programa faz conversões.
+
+>>> conversao(32)
+0.0
+
+>>> conversao(-40)
+-40.0
+"""
+
+def conversao(temperatura_fahrenheit):
+
+    """
+    Método que converte
+    
+    """
+    return 5 * ((temperatura_fahrenheit - 32) / 9)
---
+++ intro_testes/main.py
@@ -0,0 +1,11 @@
+from conversao import conversao
+
+#input
+temperatura_fahrenheit = float(input("Digite a temperatura em fahrenheit: "))
+
+#processamento
+
+temperatura_celsius = (conversao(temperatura_fahrenheit))
+
+#output
+print(f'a temperatura convertidade é {temperatura_celsius}')
